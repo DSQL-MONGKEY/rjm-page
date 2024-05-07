a931ac63e107ba4480e288b86f3ff4e0ff3d0072
Dimpfe <dimasprasetyo7696@gmail.com>
2024-05-07 14:10:46 +0700

    feat<>: hero title oncreate

--- app/container/Hero.tsx
+++ app/container/Hero.tsx
@@ -1,8 +1,20 @@
+"use client"
+
 import React from 'react'
 
 const Hero = () => {
    return (
-      <div>Hero</div>
+      <section className={`w-full bg-black h-[680px] p-10`}>
+         <div className="flex flex-col sm:flex-row justify-between">
+            <div className='flex flex-col justify-center'>
+               <h1 className='text-white text-5xl font-robotoBold'>Ratu Jaya Motors</h1>
+               <p className='text-slate-300 text-xl'>
+                  Penyok anda adalah kesan pertama kami!
+               </p>
+            </div>
+            <div className='w-[300px] h-[300px] bg-slate-500'></div>
+         </div>
+      </section>
    )
 }
 
