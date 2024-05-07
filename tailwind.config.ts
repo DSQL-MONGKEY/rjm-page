5c14014bb4072b2e0238aa12b96dd6bb2c3470b4
Dimpfe <dimasprasetyo7696@gmail.com>
2024-05-07 14:08:32 +0700

    feat<fontFamily>: add new font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Outfit } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        maison: ['maison', 'sans'],
+        outfit: ['outfit', 'sans'],
+        quicksand: ['quicksand', 'sans'],
+        poppins: ['poppins', 'sans'],
+        robotoBold: ['robotoBold', 'sans'],
+        robotoMed: ['robotoMedium', 'sans'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
